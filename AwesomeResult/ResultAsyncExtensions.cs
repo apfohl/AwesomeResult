d76c288676fbcc4a9526b723362d82cb906bc5a9
Andreas Pfohl <mail@apfohl.com>
2024-01-25 15:00:44 +0100

    Added FlatMap, Bind and LinqAsync

--- AwesomeResult/ResultAsyncExtensions.cs
+++ AwesomeResult/ResultAsyncExtensions.cs
@@ -5,22 +5,34 @@ namespace AwesomeResult
 {
     public static class ResultAsyncExtensions
     {
-        public static Task<Result<TResult>> Map<T, TResult>(this Result<T> result, Func<T, Task<TResult>> mapping,
+        public static Task<Result<TResult>> Select<T, TResult>(this Result<T> result, Func<T, Task<TResult>> mapping,
             bool continueOnCapturedContext = false) =>
             result.Select(mapping).Match(
                 async task => (await task.ConfigureAwait(continueOnCapturedContext)).Success(),
                 errors => Task.FromResult(errors.Fail<TResult>())
             );
 
-        public static async Task<Result<TResult>> Map<T, TResult>(this Task<Result<T>> result, Func<T, TResult> mapping,
-            bool continueOnCapturedContext = false) =>
+        public static async Task<Result<TResult>> Select<T, TResult>(this Task<Result<T>> result,
+            Func<T, TResult> mapping, bool continueOnCapturedContext = false) =>
             (await result.ConfigureAwait(continueOnCapturedContext)).Select(mapping);
 
-        public static async Task<Result<TResult>> Map<T, TResult>(this Task<Result<T>> result,
+        public static async Task<Result<TResult>> Select<T, TResult>(this Task<Result<T>> result,
             Func<T, Task<TResult>> mapping, bool continueOnCapturedContext = false) =>
-            await (await result.ConfigureAwait(continueOnCapturedContext)).Map(mapping, continueOnCapturedContext);
+            await (await result.ConfigureAwait(continueOnCapturedContext)).Select(mapping, continueOnCapturedContext);
 
-        public static Task<Result<TResult>> Bind<T, TResult>(this Result<T> result,
+        public static Task<Result<TResult>> Map<T, TResult>(this Result<T> result, Func<T, Task<TResult>> mapping,
+            bool continueOnCapturedContext = false) =>
+            result.Select(mapping, continueOnCapturedContext);
+
+        public static Task<Result<TResult>> Map<T, TResult>(this Task<Result<T>> result,
+            Func<T, TResult> mapping, bool continueOnCapturedContext = false) =>
+            result.Select(mapping, continueOnCapturedContext);
+
+        public static Task<Result<TResult>> Map<T, TResult>(this Task<Result<T>> result,
+            Func<T, Task<TResult>> mapping, bool continueOnCapturedContext = false) =>
+            result.Select(mapping, continueOnCapturedContext);
+
+        public static Task<Result<TResult>> SelectMany<T, TResult>(this Result<T> result,
             Func<T, Task<Result<TResult>>> mapping, bool continueOnCapturedContext = false)
         {
             if (mapping == null) throw new ArgumentNullException(nameof(mapping));
@@ -30,12 +42,37 @@ namespace AwesomeResult
                 errors => Task.FromResult(errors.Fail<TResult>()));
         }
 
-        public static async Task<Result<TResult>> Bind<T, TResult>(this Task<Result<T>> result,
+        public static async Task<Result<TResult>> SelectMany<T, TResult>(this Task<Result<T>> result,
             Func<T, Result<TResult>> mapping, bool continueOnCapturedContext = false) =>
             (await result.ConfigureAwait(continueOnCapturedContext)).SelectMany(mapping);
 
-        public static async Task<Result<TResult>> Bind<T, TResult>(this Task<Result<T>> result,
+        public static async Task<Result<TResult>> SelectMany<T, TResult>(this Task<Result<T>> result,
             Func<T, Task<Result<TResult>>> mapping, bool continueOnCapturedContext = false) =>
-            await (await result.ConfigureAwait(continueOnCapturedContext)).Bind(mapping, continueOnCapturedContext);
+            await (await result.ConfigureAwait(continueOnCapturedContext)).SelectMany(mapping,
+                continueOnCapturedContext);
+
+        public static Task<Result<TResult>> Bind<T, TResult>(this Result<T> result,
+            Func<T, Task<Result<TResult>>> mapping, bool continueOnCapturedContext = false) =>
+            result.SelectMany(mapping, continueOnCapturedContext);
+
+        public static Task<Result<TResult>> Bind<T, TResult>(this Task<Result<T>> result,
+            Func<T, Result<TResult>> mapping, bool continueOnCapturedContext = false) =>
+            result.SelectMany(mapping, continueOnCapturedContext);
+
+        public static Task<Result<TResult>> Bind<T, TResult>(this Task<Result<T>> result,
+            Func<T, Task<Result<TResult>>> mapping, bool continueOnCapturedContext = false) =>
+            result.SelectMany(mapping, continueOnCapturedContext);
+
+        public static Task<Result<TResult>> FlatMap<T, TResult>(this Result<T> result,
+            Func<T, Task<Result<TResult>>> mapping, bool continueOnCapturedContext = false) =>
+            result.SelectMany(mapping, continueOnCapturedContext);
+
+        public static Task<Result<TResult>> FlatMap<T, TResult>(this Task<Result<T>> result,
+            Func<T, Result<TResult>> mapping, bool continueOnCapturedContext = false) =>
+            result.SelectMany(mapping, continueOnCapturedContext);
+
+        public static Task<Result<TResult>> FlatMap<T, TResult>(this Task<Result<T>> result,
+            Func<T, Task<Result<TResult>>> mapping, bool continueOnCapturedContext = false) =>
+            result.SelectMany(mapping, continueOnCapturedContext);
     }
 }
---
+++ AwesomeResult/ResultLinqAsyncExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Threading.Tasks;
+
+namespace AwesomeResult
+{
+    public static class ResultLinqAsyncExtensions
+    {
+        public static Task<Result<TResult>> SelectMany<T, TCollection, TResult>(
+            this Task<Result<T>> result,
+            Func<T, Task<Result<TCollection>>> collectionTask,
+            Func<T, TCollection, TResult> selector)
+        {
+            if (collectionTask == null) throw new ArgumentNullException(nameof(collectionTask));
+            if (selector == null) throw new ArgumentNullException(nameof(selector));
+
+            return result.SelectMany(m => collectionTask(m).Select(value => selector(m, value)));
+        }
+    }
+}
